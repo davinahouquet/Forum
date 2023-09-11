50fc11182cec1897034a14ae0d4cc3ea2962b261
davinahouquet <davina.houquet@hotmail.com>
2023-09-11 11:12:22 +0200

    Form. login dans la page d'accueil

--- view/home.php
+++ view/home.php
@@ -1,7 +1,27 @@
 <h1>WELCOME !</h1>
 
+<?php
+
+if(!isset($_SESSION['user'])){
+    ?>
+    <h2>Login</h2>
+    
+    <form enctype=mutlitpart/form-data action="index.php?ctrl=security&action=login" method="post">
+    
+    <label for="email">Email</label>
+    <input type="email" name="email" required>
+    
+    <label for="password">Password</label>
+    <input type="password" name="password" required>
+    
+    <input type="submit" name="submitLogin">
+    
+</form>
+
 <p>
-    <a href="index.php?ctrl=security&action=login" class="home-connexion">LOG IN</a>
-    <span>&nbsp;-&nbsp;</span>
-    <a href="index.php?ctrl=security&action=registration" class="home-connexion">SIGN IN</a>
+    You don't have an account ? <b><a href="index.php?ctrl=security&action=registration" class="home-connexion">SIGN IN<b></a>
 </p>
+    
+<?php
+}
+?>
